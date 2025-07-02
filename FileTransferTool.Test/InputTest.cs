554e8081e055ffce0a982fc9f017413bc15bd91c
teonakuzmanovska <teona.kuzmanovska@students.finki.ukim.mk>
2025-07-03 00:45:35 +0200

    added unit test folder

---
+++ FileTransferTool.Test/InputTest.cs
@@ -0,0 +1,32 @@
+using Input = FileTransferTool.Processes.Validations.Input;
+
+namespace FileTransferTool.Test;
+
+public class InputTest
+{
+    [Test]
+    public void ValidFilePathTest()
+    {
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string testFilePath = Path.Combine(desktopPath, "TestInputFile.txt");
+
+        try
+        {
+            File.WriteAllText(testFilePath, string.Empty);
+            Assert.IsTrue(Input.IsPathValid(testFilePath, isCheckForFile: true));
+        }
+
+        finally
+        {
+            if (File.Exists(testFilePath)) File.Delete(testFilePath);
+        }
+        
+    }
+    
+    [Test]
+    public void ValidDirectoryPathTest()
+    {
+        var testDirectoryPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        Assert.IsTrue(Input.IsPathValid(testDirectoryPath, isCheckForFile: false));
+    }
+}
