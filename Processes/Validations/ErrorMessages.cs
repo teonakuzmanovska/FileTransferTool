c494cc5b93744016cc4209a27a369bef35b9de53
teonakuzmanovska <teona.kuzmanovska@students.finki.ukim.mk>
2025-07-03 00:26:40 +0200

    Initial implementation of path reader and validator

---
+++ Processes/Validations/ErrorMessages.cs
@@ -0,0 +1,24 @@
+﻿namespace FileTransferTool.Processes.Validations;
+
+public static class ErrorMessages
+{
+    public const string Default = "Processing error. Please try again.";
+    
+    public static readonly Dictionary<ErrorKey,string> ErrorMessageDict = new()
+    {
+        {ErrorKey.EmptyPath, "The path is empty."},
+        {ErrorKey.InvalidCharacters, "The path contains an invalid character."},
+        {ErrorKey.RelativePath, "Please provide the full path to the file."},
+        {ErrorKey.NonExistingFile, "The file does not exist."},
+        {ErrorKey.NonExistingDirectory, "The destination path does not exist."},
+    };
+    
+    public enum ErrorKey
+    {
+        EmptyPath,
+        InvalidCharacters,
+        RelativePath,
+        NonExistingFile,
+        NonExistingDirectory
+    }
+}
